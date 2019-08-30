35ad0ca080916cfe5daf1e0950b2ced6af51eb27
Stephen Allen <staylorallen@gmail.com>
2019-08-29 22:53:37 -0400

    stable. part 1 complete

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GT-Hwk13-bamazon
+
+## Instructions
+npm install
+create database and set corresponding information in config.json
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,4 +9,52 @@ module.exports = function(app){
     });
   });
 
+app.post('/api/update', function(req, res) {
+  db.Product.findAll({})
+  .then(function(results) {
+    res.json(results);
+  });
+})
+
+
+app.post('/api/placeOrder', function(req, res) {
+  db.Product.findAll({
+    where: {
+      id: req.body.idToSearch
+    }
+  })
+  .then(function(results) {
+    const a = results[0].stockQuantity;
+    const b = parseInt(req.body.quantity);
+    const reduceBy = a - b;
+    if(results[0].stockQuantity < req.body.quantity) {
+      res.json({message:"Ordering too many"})
+    } else {
+      db.Product.update({
+        stockQuantity: reduceBy
+      }, {
+        where: {
+          id:results[0].id
+        }
+      }
+
+    ) // end update
+      res.json({message:"Order complete!"})
+    }
+  })
+})
+
+// app.put('/api/update', function(req, res) {
+//   db.Product.update({
+//     stockQuantity:req.body.results[0] - req.body.quantity
+//   }
+//
+// ).then(function(results){
+//   res.json({message:"worked"})
+// })
+
+// })
+
+
+
 } // end module.exports
